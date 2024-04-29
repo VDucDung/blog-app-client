0312d2fa9bbc40bb61671edd55af6a44686b09f5
VDucDung <dunga3qv2@gmail.com>
2024-04-29 10:06:54 +0700

    ✅ Create article card component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -43,7 +43,7 @@ const NavItem = ({ item }) => {
           <div
             className={`${dropdown ? 'block' : 'hidden'} lg:hidden transition-all duration-500 pt-4 lg:absolute lg:bottom-0 lg:right-0 lg:transform lg:translate-y-full lg:group-hover:block w-max`}
           >
-            <ul className="bg-dark-soft lg:bg-transparent text-center flex flex-col shadow-lg rounded-lg overflow-hidden">
+            <ul className='bg-dark-soft lg:bg-transparent text-center flex flex-col shadow-lg rounded-lg overflow-hidden'>
               {item.items.map((page, index) => (
                 <a key={index} href='/' className='hover:bg-dark-light hover:text-white px-4 py-2 text-white lg:text-dark-soft'>
                   {page}
@@ -64,7 +64,7 @@ const Header = () => {
       return !curState
     })
   }
-  return <section className='sticky top-0 left-0 right-0 z-50'>
+  return <section className="sticky top-0 left-0 right-0 z-50 bg-white">
     <header className='container mx-auto px-5 flex justify-between py-4 items-center'>
       <div>
         <img className='w-16' src={images.Logo} alt='logo' />
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,10 +1,12 @@
 import MainLayout from 'components/MainLayout'
+import Articles from 'pages/container/Articles'
 import Hero from 'pages/container/Hero'
 
 const HomePage = () => {
   return (
     <MainLayout>
       <Hero />
+      <Articles />
     </MainLayout>
   )
 }
