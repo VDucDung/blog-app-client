d2e428c113cead3061a7e4e0b4fcc69ca272d503
VDucDung <dunga3qv2@gmail.com>
2024-04-30 21:18:24 +0700

    ⚡️ Implementing redux

--- src/components/SocialShareButtons.jsx
+++ src/components/SocialShareButtons.jsx
@@ -7,34 +7,34 @@ import {
 
 const SocialShareButtons = ({ url, title }) => {
   return (
-    <div className="w-full flex justify-between">
+    <div className='w-full flex justify-between'>
       <a
-        target="_blank"
-        rel="noreferrer"
+        target='_blank'
+        rel='noreferrer'
         href={`https://www.facebook.com/dialog/share?app_id=811286384205211&display=popup&href=${url}`}
       >
-        <FaFacebookSquare className="text-[#3b5998] w-12 h-auto" />
+        <FaFacebookSquare className='text-[#3b5998] w-12 h-auto' />
       </a>
       <a
-        target="_blank"
-        rel="noreferrer"
+        target='_blank'
+        rel='noreferrer'
         href={`https://twitter.com/intent/tweet?url=${url}`}
       >
-        <FaTwitterSquare className="text-[#00acee] w-12 h-auto" />
+        <FaTwitterSquare className='text-[#00acee] w-12 h-auto' />
       </a>
       <a
-        target="_blank"
-        rel="noreferrer"
+        target='_blank'
+        rel='noreferrer'
         href={`http://www.reddit.com/submit?url=${url}&title=${title}`}
       >
-        <FaRedditSquare className="text-[#ff4500] w-12 h-auto" />
+        <FaRedditSquare className='text-[#ff4500] w-12 h-auto' />
       </a>
       <a
-        target="_blank"
-        rel="noreferrer"
+        target='_blank'
+        rel='noreferrer'
         href={`https://api.whatsapp.com/send/?text=${url}`}
       >
-        <FaWhatsappSquare className="text-[#25D366] w-12 h-auto" />
+        <FaWhatsappSquare className='text-[#25D366] w-12 h-auto' />
       </a>
     </div>
   )
--- src/pages/home/container/Articles.jsx
+++ src/pages/home/container/Articles.jsx
@@ -1,6 +1,14 @@
 import ArticleCard from 'components/ArticleCard'
 import { FaArrowRight } from 'react-icons/fa'
+import { useSelector, useDispatch } from 'react-redux'
+import { changeCount } from 'store/actions/countActions'
 const Articles = () => {
+  const dispatch = useDispatch()
+  const count = useSelector((state) => state.count)
+
+  const countChangeHandler = (type) => {
+    dispatch(changeCount(type))
+  }
   return (
     <section className='flex flex-col container mx-auto px-5 py-10'>
       <div className=' flex flex-wrap md:gap-x-5 gap-y-5 pb-10'>
@@ -11,6 +19,11 @@ const Articles = () => {
         <span>More articles</span>
         <FaArrowRight className='w-3 h-3' />
       </button>
+      <div className='mt-2 flex items-center gap-x-5'>
+        <button onClick={() => countChangeHandler('DECREASE')}>decrease</button>
+        {count.number}
+        <button onClick={() => countChangeHandler('INCREASE')}>increase</button>
+      </div>
     </section>
   )
 }
