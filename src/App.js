70fc260f9d204c21de5995169a27776e2ca59592
VDucDung <dunga3qv2@gmail.com>
2024-04-30 21:29:33 +0700

    ✅ Add register page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import 'App.css'
 import HomePage from 'pages/home/HomePage'
 import ArticleDetailPage from './pages/articleDetail/ArticleDetailPage'
 import { Routes, Route } from 'react-router-dom'
+import RegisterPage from 'pages/register/RegisterPage'
 function App() {
   return (
     <div className='App font-opensans'>
       <Routes>
         <Route index path='/' element={<HomePage />} />
         <Route path='/blog/:id' element={<ArticleDetailPage />} />
+        <Route path='/register' element={<RegisterPage />} />
       </Routes>
     </div>
   )
