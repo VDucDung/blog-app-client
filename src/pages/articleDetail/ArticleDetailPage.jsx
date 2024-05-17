2c8f03a20d713a586611e0f392828ced2873a7c8
VDucDung <dunga3qv2@gmail.com>
2024-05-17 23:31:12 +0700

    ✨ Creating loading & error state

--- src/pages/articleDetail/ArticleDetailPage.jsx
+++ src/pages/articleDetail/ArticleDetailPage.jsx
@@ -1,22 +1,23 @@
-import { useState, useEffect } from 'react'
-import { Link, useParams } from 'react-router-dom'
-import { generateHTML } from '@tiptap/html'
-import Bold from '@tiptap/extension-bold'
-import Document from '@tiptap/extension-document'
-import Paragraph from '@tiptap/extension-paragraph'
-import Text from '@tiptap/extension-text'
-import Italic from '@tiptap/extension-italic'
 import parse from 'html-react-parser'
-
-import BreadCrumbs from 'components/BreadCrumbs'
-import MainLayout from 'components/MainLayout'
-import { images } from 'constants'
-import SuggestedPosts from './container/SuggestedPosts'
-import CommentsContainer from 'components/comments/CommentsContainer'
-import SocialShareButtons from 'components/SocialShareButtons'
-
+import Text from '@tiptap/extension-text'
+import Bold from '@tiptap/extension-bold'
+import { useState, useEffect } from 'react'
+import { generateHTML } from '@tiptap/html'
+import Italic from '@tiptap/extension-italic'
 import { useQuery } from '@tanstack/react-query'
+import Document from '@tiptap/extension-document'
+import { Link, useParams } from 'react-router-dom'
+import Paragraph from '@tiptap/extension-paragraph'
+
+import { images } from 'constants'
+import MainLayout from 'components/MainLayout'
+import BreadCrumbs from 'components/BreadCrumbs'
 import { getSinglePost } from 'services/index/posts'
+import SuggestedPosts from './container/SuggestedPosts'
+import ErrorMessage from 'components/ErrorMessage'
+import SocialShareButtons from 'components/SocialShareButtons'
+import CommentsContainer from 'components/comments/CommentsContainer'
+import ArticleDetailSkeleton from './components/ArticleDetailSkeleton'
 
 const postsData = [
   {
@@ -42,7 +43,7 @@ const ArticleDetailPage = () => {
   const [breadCrumbsData, setbreadCrumbsData] = useState([])
   const [body, setBody] = useState(null)
 
-  const { data } = useQuery({
+  const { data, isLoading, isError } = useQuery({
     queryFn: () => getSinglePost({ slug }),
     queryKey: ['blog', slug],
     onSuccess: (data) => {
@@ -68,52 +69,62 @@ const ArticleDetailPage = () => {
 
   return (
     <MainLayout>
-      <section className='container mx-auto max-w-5xl flex flex-col px-5 py-5 lg:flex-row lg:gap-x-5 lg:items-start'>
-        <article className='flex-1'>
-          <BreadCrumbs data={breadCrumbsData} />
-          <img
-            className='rounded-xl w-full'
-            src={
-              data?.data?.image
-                ? data?.data?.image
-                : images.samplePostImage
-            }
-            alt={data?.data?.title}
-          />
-          <div className='mt-4 flex gap-2'>
-            {data?.data?.categories ? data?.data?.categories.map((category) => (
-              <Link key={category._id} to={`/blog?category=${category.name}`} className='text-primary text-sm font-roboto inline-block md:text-base'>
-                {category.name}
-              </Link>)) : null}
-          </div>
-          <h1 className='text-xl font-medium font-roboto mt-4 text-dark-hard md:text-[26px]'>
-            {data?.data?.title}
-          </h1>
-          <div className='mt-4 prose prose-sm sm:prose-base'>{body}</div>
-          <CommentsContainer className='mt-10' logginedUserId='a' />
-        </article>
-        <div>
-          <SuggestedPosts
-            header='Latest Article'
-            posts={postsData}
-            tags={tagsData}
-            className='mt-8 lg:mt-0 lg:max-w-xs'
-          />
-          <div className='mt-7'>
-            <h2 className='font-roboto font-medium text-dark-hard mb-4 md:text-xl'>
-              Share on:
-            </h2>
-            <SocialShareButtons
-              url={encodeURI(
-                'https://moonfo.com/post/client-side-and-server-side-explanation'
-              )}
-              title={encodeURIComponent(
-                'Client-side and Server-side explanation'
-              )}
+      {isLoading ? (
+        <ArticleDetailSkeleton />
+      ) : isError ? (
+        <ErrorMessage message='Could not fetch the post detail' />
+      ) : (
+        <section className='container mx-auto max-w-5xl flex flex-col px-5 py-5 lg:flex-row lg:gap-x-5 lg:items-start'>
+          <article className='flex-1'>
+            <BreadCrumbs data={breadCrumbsData} />
+            <img
+              className='rounded-xl w-full'
+              src={
+                data?.data?.photo
+                  ? data?.data?.photo
+                  : images.samplePostImage
+              }
+              alt={data?.data?.title}
             />
+            <div className='mt-4 flex gap-2'>
+              {data?.data?.categories.map((category) => (
+                <Link key={category._id}
+                  to={`/blog?category=${category.name}`}
+                  className='text-primary text-sm font-roboto inline-block md:text-base'
+                >
+                  {category.name}
+                </Link>
+              ))}
+            </div>
+            <h1 className='text-xl font-medium font-roboto mt-4 text-dark-hard md:text-[26px]'>
+              {data?.data?.title}
+            </h1>
+            <div className='mt-4 prose prose-sm sm:prose-base'>{body}</div>
+            <CommentsContainer className='mt-10' logginedUserId='a' />
+          </article>
+          <div>
+            <SuggestedPosts
+              header='Latest Article'
+              posts={postsData}
+              tags={tagsData}
+              className='mt-8 lg:mt-0 lg:max-w-xs'
+            />
+            <div className='mt-7'>
+              <h2 className='font-roboto font-medium text-dark-hard mb-4 md:text-xl'>
+                Share on:
+              </h2>
+              <SocialShareButtons
+                url={encodeURI(
+                  'https://moonfo.com/post/client-side-and-server-side-explanation'
+                )}
+                title={encodeURIComponent(
+                  'Client-side and Server-side explanation'
+                )}
+              />
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      )}
     </MainLayout>
   )
 }
--- src/pages/home/container/Articles.jsx
+++ src/pages/home/container/Articles.jsx
@@ -4,6 +4,8 @@ import { useQuery } from '@tanstack/react-query'
 
 import ArticleCard from 'components/ArticleCard'
 import { getAllPosts } from 'services/index/posts'
+import ErrorMessage from 'components/ErrorMessage'
+import ArticleCardSkeleton from 'components/ArticleCardSkeleton'
 
 const Articles = () => {
   const { data, isLoading, isError } = useQuery({
@@ -17,15 +19,24 @@ const Articles = () => {
   return (
     <section className='flex flex-col container mx-auto px-5 py-10'>
       <div className=' flex flex-wrap md:gap-x-5 gap-y-5 pb-10'>
-        {!isLoading &&
-          !isError &&
+        {isLoading ? (
+          [...Array(3)].map((item, index) => (
+            <ArticleCardSkeleton
+              key={index}
+              className='w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]'
+            />
+          ))
+        ) : isError ? (
+          <ErrorMessage message='Could not fetch the posts data' />
+        ) : (
           data?.data.map((post) => (
             <ArticleCard
               key={post._id}
               post={post}
               className='w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]'
             />
-          ))}
+          ))
+        )}
       </div>
       <button className='mx-auto flex items-center gap-x-2 font-bold text-primary border-2 border-primary px-6 py-3 rounded-lg'>
         <span>More articles</span>
