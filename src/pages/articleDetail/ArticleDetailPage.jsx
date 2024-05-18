3dacbd8c40a971e03f735f034b935d16e010b493
VDucDung <dunga3qv2@gmail.com>
2024-05-18 23:09:08 +0700

    ✨ Update the data of the sidebar of the article detail page

--- src/pages/articleDetail/ArticleDetailPage.jsx
+++ src/pages/articleDetail/ArticleDetailPage.jsx
@@ -13,31 +13,12 @@ import Paragraph from '@tiptap/extension-paragraph'
 import { images } from 'constants'
 import MainLayout from 'components/MainLayout'
 import BreadCrumbs from 'components/BreadCrumbs'
-import { getSinglePost } from 'services/index/posts'
+import { getAllPosts, getSinglePost } from 'services/index/posts'
 import SuggestedPosts from './container/SuggestedPosts'
 import ErrorMessage from 'components/ErrorMessage'
 import SocialShareButtons from 'components/SocialShareButtons'
 import CommentsContainer from 'components/comments/CommentsContainer'
 import ArticleDetailSkeleton from './components/ArticleDetailSkeleton'
-
-const postsData = [
-  {
-    _id: '1',
-    image: images.Post1Image,
-    title: 'Help children get better education',
-    createdAt: '2023-01-28T15:35:53.607+0000'
-  }
-]
-
-const tagsData = [
-  'Medical',
-  'Lifestyle',
-  'Learn',
-  'Healthy',
-  'Food',
-  'Diet',
-  'Education'
-]
 
 const ArticleDetailPage = () => {
   const { id: slug } = useParams()
@@ -68,6 +49,13 @@ const ArticleDetailPage = () => {
       )
     }
   }, [data])
+
+  const { data: postsData } = useQuery({
+    queryFn: () => getAllPosts(),
+    queryKey: ['posts']
+  })
+  console.log(postsData)
+
   return (
     <MainLayout>
       {isLoading ? (
@@ -111,8 +99,8 @@ const ArticleDetailPage = () => {
           <div>
             <SuggestedPosts
               header='Latest Article'
-              posts={postsData}
-              tags={tagsData}
+              posts={postsData?.data}
+              tags={data?.data?.tags}
               className='mt-8 lg:mt-0 lg:max-w-xs'
             />
             <div className='mt-7'>
@@ -120,12 +108,8 @@ const ArticleDetailPage = () => {
                 Share on:
               </h2>
               <SocialShareButtons
-                url={encodeURI(
-                  'https://moonfo.com/post/client-side-and-server-side-explanation'
-                )}
-                title={encodeURIComponent(
-                  'Client-side and Server-side explanation'
-                )}
+                url={encodeURI(window.location.href)}
+                title={encodeURIComponent(data?.data?.title)}
               />
             </div>
           </div>
